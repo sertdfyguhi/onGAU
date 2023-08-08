3551e741a6be89241f7c9d8ffbb7d4d1f344fd89
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-08-08 14:03:56 +0800

    add metadata transfer to esrgan script

--- scripts/esrgan.py
+++ scripts/esrgan.py
@@ -1,12 +1,23 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../onGAU'))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../onGAU"))
 
 from argparse import ArgumentParser
 from imagen import RealESRGAN
+from PIL.PngImagePlugin import PngInfo
 from PIL import Image
 import time
+
+
+def dict_to_PngInfo(info: dict):
+    pnginfo = PngInfo()
+
+    for key in info:
+        pnginfo.add_text(key, info[key])
+
+    return pnginfo
+
 
 parser = ArgumentParser("ESRGAN Upscale Script")
 parser.add_argument("--model", required=True, type=str)
@@ -25,11 +36,14 @@ if args.tile_size:
 print("Upscaling image...")
 
 start = time.time()
-image = esrgan.upscale_image(Image.open(args.image), args.upscale)
+image = Image.open(args.image)
+upscaled = esrgan.upscale_image(image, args.upscale)
 
 print("Saving image...")
 
-image.image.save(args.outpath)
+upscaled.image.save(
+    args.outpath, exif=image.getexif(), pnginfo=dict_to_PngInfo(image.info)
+)
 
 print(f"Successfully saved image to {args.outpath}.")
 print(f"Done in {time.time() - start:.1f}s.")
