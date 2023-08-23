258a28281cf9150a97d0aa8ff9ebbb2c2cb8af3e
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-08-23 20:50:08 +0800

    fix english in install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 pip install -r requirements.txt
 mkdir saves ongau/models
 
-echo "Installing all required packages done!\nYou should now configure your model in onGAU/config.py and then run onGAU/main.py!\nThanks for using onGAU!"
+echo "onGAU installed!\nYou can now configure default settings in onGAU/config.py and run onGAU/main.py!\nThanks for using onGAU!"
