675a007aa840764c39e068614f7c33b0d5bab1e4
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-08-26 15:00:36 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 pip install -r requirements.txt
-mkdir saves ongau/models
+mkdir saves onGAU/models
 
-echo "onGAU installed!\nYou can now configure default settings in onGAU/config.py and run onGAU/main.py!\nThanks for using onGAU!"
+echo "onGAU installed!"
+echo "You can now configure default settings in onGAU/config.py and run onGAU/main.py!"
+echo "Thanks for using onGAU!"
