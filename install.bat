b0c56737fa5a2db29038cdbb134a3fbbac334cb1
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-08-26 14:59:57 +0800

    add install batch script

---
+++ install.bat
@@ -0,0 +1,6 @@
+pip install -r requirements.txt
+mkdir saves onGAU\models
+
+echo "onGAU installed!"
+echo "You can now configure default settings in onGAU/config.py and run onGAU/main.py!"
+echo "Thanks for using onGAU!"
