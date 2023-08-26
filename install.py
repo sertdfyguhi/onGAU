53814465b0f89de46cdd87a5bb3e54474e35ade1
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-08-26 23:17:44 +0800

    fix writing to config.py

--- install.py
+++ install.py
@@ -12,10 +12,11 @@ os.mkdir(os.path.join(root, "onGAU", "models"))
 print("What should be the default model when starting onGAU?")
 model = input("Path to model: ")
 
-with open(os.path.join(root, "onGAU", "config.py"), "w+") as f:
+with open(os.path.join(root, "onGAU", "config.py"), "r+") as f:
     contents = f.read()
-    contents = contents.replace('"%default_model%"', repr(model))
+    contents = contents.replace('"%default_model%"', repr(model), 1)
 
+    f.truncate(0)
     f.write(contents)
 
 print("Finished onGAU setup! Happy generating!")
